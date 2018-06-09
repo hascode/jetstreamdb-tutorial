3ec6b1994973f7baefd3b0b625b40a7abd6469f3
Micha Kops <micha.kops@gmail.com>
2018-06-09 20:49:32 +0200

    db directory modified

--- src/main/java/com/hascode/tutorial/ArticleApp.java
+++ src/main/java/com/hascode/tutorial/ArticleApp.java
@@ -2,25 +2,33 @@ package com.hascode.tutorial;
 
 import com.jetstreamdb.JetstreamDBInstance;
 import java.io.Console;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class ArticleApp {
 
-  private static final String USAGE = "Enter a command 'l': list, 'a': add, 'q': quit\n";
+  private static final String USAGE = "Enter a command 'l': list, 'a': add, 'q': quit 'd': db info\n";
 
-  private JetstreamDBInstance<RootData> db = JetstreamDBInstance.New("my-articles-db ", RootData.class);
+  private JetstreamDBInstance<RootData> db;
+
+  public ArticleApp() {
+    db = JetstreamDBInstance.New("my-articles-db ", RootData.class);
+    Path databaseDirectory = Paths.get("").resolve("article-db");
+    System.out.printf("creating database directory in %s%n", databaseDirectory.toAbsolutePath());
+    db.configuration().properties().setStorageDirectory(databaseDirectory.toFile());
+  }
 
   public static void main(String[] args) {
     new ArticleApp().run();
   }
 
   public void run() {
-
-    Console console = System.console();
-    prompt(console);
+    prompt();
   }
 
-  private void prompt(Console console) {
+  private void prompt() {
+    Console console = System.console();
     System.out.println(USAGE);
     String command = console.readLine();
     switch (command) {
@@ -32,7 +40,7 @@ public class ArticleApp {
         List<Article> articles = db.root().getArticles();
         System.out.printf("%d articles found:%n", articles.size());
         articles.forEach(System.out::println);
-        prompt(console);
+        prompt();
         break;
       case "a":
         System.out.println("Please enter a title");
@@ -40,11 +48,16 @@ public class ArticleApp {
         List<Article> update = db.root().getArticles();
         update.add(new Article(title));
         db.store(update);
-        prompt(console);
+        prompt();
+        break;
+      case "d":
+        System.out.printf("basedir: %s, name: %s%n", db.configuration().getBaseDir(),
+            db.configuration().name());
+        prompt();
         break;
       default:
         System.out.println("invalid command");
-        prompt(console);
+        prompt();
     }
   }
 
